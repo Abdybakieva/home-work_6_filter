be7911819d377f4721b3b4c016925ba1cd004e9e
Abdybakieva <aselabdybakievaa@exampele.com>
2023-01-27 17:54:13 +0600

    h-w_6-filter

--- src/components/ExpenseFilter/ExpenseFilter.jsx
+++ src/components/ExpenseFilter/ExpenseFilter.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import "./ExpenseFilter.css";
 
-const ExpenseFilter = ({value, onChange}) => {
+const ExpenseFilter = ({ value, onChange }) => {
   return (
     <ExpenseFiltere>
       <ExpenseFilterControl>
         <expenseFilterControlLabel label htmlFor="">
           Filter by year
         </expenseFilterControlLabel>
-        <select    onChange={onChange} value={value}>
+        <select onChange={onChange} value={value}>
           <option value="2023">2023</option>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
@@ -19,9 +19,9 @@ const ExpenseFilter = ({value, onChange}) => {
       </ExpenseFilterControl>
     </ExpenseFiltere>
   );
-}
+};
 
-export default ExpenseFilter
+export default ExpenseFilter;
 const ExpenseFiltere = styled.div`
   color: white;
   padding: 0 1rem;
@@ -39,10 +39,10 @@ const expenseFilterControlLabel = styled.label`
   padding: 10px;
 `;
 
-const expenseFilterControlSelect=styled.select`
+const expenseFilterControlSelect = styled.select`
   width: 100px;
-    height: 36px;
-    border-radius: 7px;
-    text-align: center;
-    font-size: 18px;
-`
+  height: 36px;
+  border-radius: 7px;
+  text-align: center;
+  font-size: 18px;
+`;
